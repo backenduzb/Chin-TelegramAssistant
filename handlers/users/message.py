ff180f029e16406fa3db8ccc035d0f65255b287f
Javohir <qobiljonsharipov33@gmail.com>
2026-02-19 19:34:47 +0500

    Fixing parse mode bug

--- handlers/users/message.py
+++ handlers/users/message.py
@@ -16,4 +16,5 @@ async def response_with_ai(message: types.Message, bot: Bot):
         bot=bot, chat_id=message.from_user.id, action=ChatAction.RECORD_VOICE
     ):
         ai_response = await get_request_data(str(message.text))
+    await message.answer(ai_response)
     await write(text=ai_response, message=message)
