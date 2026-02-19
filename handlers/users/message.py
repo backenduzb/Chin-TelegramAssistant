9c06ec5319e82324c15fe94a968a27668c4d0a4b
Javohir <qobiljonsharipov33@gmail.com>
2026-02-19 19:34:55 +0500

    Fixing parse mode bug

--- handlers/users/message.py
+++ handlers/users/message.py
@@ -1,3 +1,4 @@
+from ast import parse
 from aiogram import Router, types, Bot
 from aiogram.enums import ChatAction
 from aiogram.utils.chat_action import ChatActionSender
@@ -16,5 +17,5 @@ async def response_with_ai(message: types.Message, bot: Bot):
         bot=bot, chat_id=message.from_user.id, action=ChatAction.RECORD_VOICE
     ):
         ai_response = await get_request_data(str(message.text))
-    await message.answer(ai_response)
+    await message.answer(ai_response, parse_mode="markdown")
     await write(text=ai_response, message=message)
